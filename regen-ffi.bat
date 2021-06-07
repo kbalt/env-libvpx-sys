bf594c009989c56114b569f1fd66a23a233ad6df
Andrew Straw <strawman@astraw.com>
2021-06-07 12:02:54 +0200

    remove unused environment var

--- regen-ffi.bat
+++ regen-ffi.bat
@@ -4,7 +4,6 @@ set VPX_STATIC=1
 set VPX_VERSION=1.10.0
 set VPX_LIB_DIR=%HomeDrive%%HomePath%\libvpx_v1.10.0_msvc16\lib\x64
 set VPX_INCLUDE_DIR=%HomeDrive%%HomePath%\libvpx_v1.10.0_msvc16\include
-SET VPX_NO_PKG_CONFIG=1
 
 @REM Download llvm from https://releases.llvm.org/download.html
 set LIBCLANG_PATH=C:\Program Files\LLVM\bin
