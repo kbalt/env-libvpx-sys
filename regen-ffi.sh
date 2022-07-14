fd085775a2235d4905510be90e5c449e167db409
Andrew Straw <strawman@astraw.com>
2022-07-14 08:17:19 +0200

    fix script

--- regen-ffi.sh
+++ regen-ffi.sh
@@ -3,4 +3,4 @@ set -o errexit
 
 touch build.rs
 cargo build --no-default-features --features=generate
-echo "Generated `<OUT_DIR>/ffi.rs`. You may want to copy this into `generated/` with an appropriate name."
+echo "Generated '<OUT_DIR>/ffi.rs'. You may want to copy this into 'generated/' with an appropriate name."
